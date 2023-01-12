856429829571499220908c834da9844324729e07
hi-rev <hirev05@gmail.com>
2023-01-12 18:15:56 +0900

    emaillist

--- jdbc-practice/src/main/java/emaillist/dao/EmaillistDao.java
+++ jdbc-practice/src/main/java/emaillist/dao/EmaillistDao.java
@@ -127,7 +127,7 @@ public class EmaillistDao {
 		
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mariadb://192.168.10.125:3307/webdb?charset=utf8";
+			String url = "jdbc:mariadb://192.168.10.106:3307/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패: " + e);
