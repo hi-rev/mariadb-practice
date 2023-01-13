dd2c7eeb991b2a1a08c90a0115767d0ab47e38c8
hi-rev <hirev05@gmail.com>
2023-01-13 18:12:50 +0900

    bookmall #member

--- jdbc-practice/src/main/java/bookmall/dao/MemberDao.java
+++ jdbc-practice/src/main/java/bookmall/dao/MemberDao.java
@@ -1,5 +1,113 @@
 package bookmall.dao;
 
-public class MemberDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import bookmall.vo.MemberVo;
+
+public class MemberDao {
+	
+	public List<MemberVo> findAll() {
+		List<MemberVo> result = new ArrayList<>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql = "select * from member;";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String phone = rs.getString(3);
+				String email = rs.getString(4);
+				String password = rs.getString(5);
+				
+				MemberVo vo = new MemberVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setPhone(phone);
+				vo.setEmail(email);
+				vo.setPassword(password);
+				
+				result.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public void insert(MemberVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			// no는 auto_increment 속성을 사용하였기 때문에 null 값 넣어줌
+			// 자동으로 값 1씩 증가
+			String sql = "insert into member(no, name, phone, email, password) values(null, ?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPhone());
+			pstmt.setString(3, vo.getEmail());
+			pstmt.setString(4, vo.getPassword());
+			
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.10.106:3307/bookmall?charset=utf8";
+//			String url = "jdbc:mariadb://172.30.1.2:3307/bookmall?charset=utf8";
+			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		}
+		return conn;
+	}
 }
--- jdbc-practice/src/main/java/bookmall/dao/test/MemberDaoTest.java
+++ jdbc-practice/src/main/java/bookmall/dao/test/MemberDaoTest.java
@@ -1,10 +1,40 @@
 package bookmall.dao.test;
 
+import java.util.List;
+
+import bookmall.dao.MemberDao;
+import bookmall.vo.MemberVo;
+
 public class MemberDaoTest {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		testInsert();
+		testFindAll();
+	}
 
+	private static void testInsert() {
+		MemberVo vo = null;
+		MemberDao dao = new MemberDao();
+		
+		vo = new MemberVo();
+		vo.setName("둘리");
+		vo.setEmail("qwer@gmail.com");
+		vo.setPhone("010-0000-0000");
+		vo.setPassword("qwer");
+		dao.insert(vo);
+		
+		vo = new MemberVo();
+		vo.setName("또치");
+		vo.setEmail("asdf@gmail.com");
+		vo.setPhone("010-1111-1111");
+		vo.setPassword("asdf");
+		dao.insert(vo);
 	}
 	
+	private static void testFindAll() {
+		List<MemberVo> list = new MemberDao().findAll();
+		for (MemberVo vo: list) {
+			System.out.println(vo);
+		}
+	}
 }
